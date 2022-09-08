ed8bb3fbba80b41a05f079da969092c0c6df1772
AndreyGalchevski <andreygalchevski@gmail.com>
2022-09-08 23:02:09 +0300

    update members page

--- data/members.ts
+++ data/members.ts
@@ -1,43 +1,37 @@
 export interface Member {
   name: string;
   instrument: string;
-  info: string;
   image: string;
   imageNG: string;
 }
 
 const members: Array<Member> = [
   {
-    name: "Dmitry Samoilov (Demon)",
-    instrument: "Vocals",
-    info: "Pure Evil",
-    image:
-      "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571775978/strident/members/production/Dmitry%20Samoilov%20%28Demon%29.jpg",
-    imageNG:
-      "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571775979/strident/members/production/Dmitry%20Samoilov%20%28Demon%29_ng.webp",
-  },
-  {
     name: "Metalych",
-    instrument: "Guitar",
-    info: "Shredder From Hell",
+    instrument: "Lead Guitar and Vocals",
     image:
       "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571334257/strident/members/production/misha.jpg",
     imageNG:
       "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571391848/strident/members/production/misha_ng.webp",
   },
   {
-    name: "Artem Apekishev",
+    name: "Yaniv",
+    instrument: "Guitar",
+    image:
+      "https://res.cloudinary.com/dqvimfd8b/image/upload/v1662666920/strident/members/production/yaniv.jpg",
+    imageNG: "",
+  },
+  {
+    name: "Artem",
     instrument: "Bass Guitar",
-    info: "Bone Crushing Heaviness",
     image:
       "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571394214/strident/members/production/Artem.jpg",
     imageNG:
       "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571394214/strident/members/production/Artem_ng.webp",
   },
   {
-    name: "Andrey Galchevski",
+    name: "Andrey",
     instrument: "Drums",
-    info: "Drum Machine",
     image:
       "https://res.cloudinary.com/dqvimfd8b/image/upload/v1571394243/strident/members/production/Andrey.jpg",
     imageNG:
