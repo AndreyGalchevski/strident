a44716780af54970bef85778d9f3ac3c249e175c
AndreyGalchevski <andreygalchevski@gmail.com>
2024-12-13 14:26:59 +0200

    Add missing album

--- data/albums.ts
+++ data/albums.ts
@@ -6,9 +6,9 @@ export interface Album {
 
 const albums: Album[] = [
   {
-    name: 'Paranoia of the Tyrant',
-    url: 'https://open.spotify.com/embed/album/24it76wRmx6A2p3e3QxUBB',
-    year: 2023,
+    name: 'Budapest Never Sleeps',
+    url: 'https://open.spotify.com/embed/album/3YNN7pCpybkk0yNPHS6jk4',
+    year: 2024,
   },
   {
     name: 'Paranoia of the Tyrant',
